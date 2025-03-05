d6a175e965f76ddde499e0999413141731977a49
MehtaSaket <saket.mehta.civ22@itbhu.ac.in>
2025-03-06 02:11:10 +0530

    CA form added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -146,7 +146,7 @@ const NavBar = ({ AllAuth }) => {
 						<li className="nav-item px-2 mx-2">
 							<Link
 								className="nav-link"
-								to="https://docs.google.com/forms/u/1/d/1L8DOBUhyZBMO3t7PxJue6VR8y6G2K7M5v_WsYcewWHs/edit?usp=sharing_eip_m&ts=65b66063"
+								to="https://forms.gle/oaFRmu7GhyRPuwqy6"
 							>
 								CA
 							</Link>

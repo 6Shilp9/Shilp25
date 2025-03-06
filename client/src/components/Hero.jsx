af8a9d1cafbadd2d8f51c41dfeed1f6c4e70cce7
SumitDutta007 <sumitdutta2810@gmail.com>
2025-03-07 00:09:29 +0530

    Hero section reveal effect added

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -9,16 +9,22 @@ function Hero() {
   return (
     <div className="hero-container" id="home">
       <div className="hero-content">
-        <Fade top delay={400}>
+        <Fade left>
+        <div className="hero-left"></div>
+        </Fade>
+        <Fade right>
+        <div className="hero-right"></div>
+        </Fade>
+        <Fade top delay={1400}>
         <h1 className="hero-title">SHILP'25</h1>
         <h2 className="hero-subtitle">Civil Engineering Society</h2>
         </Fade>
-        <Fade right>
+        <Fade delay={1000}>
         <img src={ShilpLogo} alt="Shilp Logo" className="hero-logo" />
         </Fade>
       </div>
       <div className="hero-bottom">
-        <Fade bottom delay = {800}>
+        <Fade bottom delay = {1600}>
         <h3 className="hero-date">11-13 April, 2025</h3>
         <div className="countdown-timer">
           <Timer />

64c52e81522805f87f73b3898a1f7886340c8b86
SumitDutta007 <sumitdutta2810@gmail.com>
2025-04-09 20:39:44 +0530

    accommodation policies added

--- client/src/components/Accommodation/AccommodationPolicies.jsx
+++ client/src/components/Accommodation/AccommodationPolicies.jsx
@@ -7,7 +7,28 @@ function AccommodationPolicies() {
     <div className='outer-body accommodationPolicies-outer-body'>
         <Zoom>
         <div className="inner-body accommodationPolicies-inner-body">
-          <h2>Would be released soon ...</h2>
+        Guests will be s upplied with mattresses and additional amenities such as pillows,
+ mattress covers. 
+Upon checkouts, guests are expected to return all provided items in good condition
+ for seamless operations.
+ All participants should return the hostel before 10 pm. No entry will be provided 
+       after closing of the hostel gates. 
+Random checks will be conducted to prevent unauthorized stays on campus. Failure
+ to present electronic or paper lodging receipts during these checks will result in
+ penalties and disqualification for the respective teams. 
+All guests must possess valid physical student photo ID proofs duly authorized by
+ their respective colleges, accompanied by a physical government-issued ID card, and
+ maintain possession of these documents at all times. Entry to the campus will strictly
+ be denied in the absence of a valid ID card. 
+The campus strictly prohibits the presence of alcohol, drugs, sharp objects,
+ explosives, and any other items deemed hazardous. In case of disputes, the  final
+ decision rests with the Security team. 
+Shilp and IIT (BHU) Varanasi will not assume responsibility for any mishaps occurring
+ during the stay. No outside vehicles will be allowed within the campus premises
+ during the fest. 
+Accommodation in the common halls of hostels at IIT (BHU) Varanasi will be
+ provided from 11th April 2024 (07:00 AM) to 14th April 2024 (up to 7:00 AM). 
+Note: Separate accommodation will be provided for boys and girls.
         </div>
         </Zoom>
     </div>
